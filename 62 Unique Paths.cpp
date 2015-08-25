2ce05f822723643089b6f4651b78dc326bcaa6bf
4solo <34310696@qq.com>
2015-08-25 00:11:51 -0700

    problem 62

---
+++ 62 Unique Paths.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(n==1||m==1)
+        {
+            return 1;
+        }
+        int ha;
+        if(m>n)
+            ha=n;
+        else
+            ha=m;
+        vector<vector<int>>result (m,vector<int>(n));
+        for(int i=0;i<m;i++)
+        {
+            result[i][0]=1;
+        }
+        for(int j=0;j<n;j++)
+        {
+            result[0][j]=1;
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                result[i][j]=result[i-1][j]+result[i][j-1];
+            }
+        }
+        return result[m-1][n-1];
+    }
+};
